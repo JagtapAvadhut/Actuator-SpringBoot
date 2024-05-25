304a9e1a48a961d9439152e5b71370a667af198e
avadhut.jagtap <avadhut.jagtap@indicchain.com>
2024-05-25 16:44:41 +0530

    first commit

---
+++ src/test/java/comspring/actuator/ActuatorSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package comspring.actuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
